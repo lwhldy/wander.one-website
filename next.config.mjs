35347b26975d99b9e9a4b82cadf634dc2e267547
lwhldy <20550490+lwhldy@users.noreply.github.com>
2026-01-18 08:24:06 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // 允许生产构建在存在 ESLint 错误时完成
+    ignoreDuringBuilds: true,
+  },
+};
+
 
 export default nextConfig;
